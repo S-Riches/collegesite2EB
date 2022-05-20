010fbb80dfeb04628412e2e67702be5c26bc78cb
Sam R <samuelricheswork@gmail.com>
2022-05-20 20:06:13 +0100

    added some code to zoom in

--- func.js
+++ func.js
@@ -1,1 +1,15 @@
-// empty atm but i will use this if needed
+// zoom feature
+// note that this practically only zooms out as the way ive designed the website uses a grid system where the ads stay the same size, a revision to this would be to change the size of the ads when zoomed
+
+function zoomIn()
+{
+    var map = document.getElementById("map");
+    var current = map.clientWidth;
+    map.style.width = (current + 100) + "px"; 
+}
+function zoomOut()
+{
+    var map = document.getElementById("map");
+    var current = map.clientWidth;
+    map.style.width = (current - 100) + "px"; 
+}
